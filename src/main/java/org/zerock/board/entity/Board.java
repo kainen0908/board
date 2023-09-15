aae7658628aaabf99f22f255ad6458c4308b3049
ksh <ksh.k5716@gmail.com>
2023-09-15 11:51:05 +0900

    230915 3교시

--- src/main/java/org/zerock/board/entity/Board.java
+++ src/main/java/org/zerock/board/entity/Board.java
@@ -9,13 +9,13 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@ToString
+@ToString(exclude = "writer")
 public class Board extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bno;
     private String title;
     private String content;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Member writer;
 }
